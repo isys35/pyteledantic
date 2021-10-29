c75e190aa0d49c1d10cbaf626676406923d0463a
isys35 <isys35@mail.ru>
2021-10-29 13:57:21 +0300

    add wheel

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from os import path
+
+import setuptools
 from setuptools import setup
 
 with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
@@ -9,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pyteledantic',
-    version='0.0.1',
+    version='0.0.7',
     description='Pydantic models for Telegram Bot API',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -26,6 +28,7 @@ setup(
     keywords='pydantic telegram api bot',
     python_requires='>=3.9, <4',
     install_requires=required_packages,
+    packages=setuptools.find_packages(),
     project_urls={
         'Source': 'https://github.com/isys35/pyteledantic',
     },
