65c858be3cfb1ba92f527141f1fcedf3a4a9f22e
root <admin@example.com>
2022-03-12 11:52:48 +0300

    🛠️ DEBUG: problem with proxy

--- pyteledantic/methods.py
+++ pyteledantic/methods.py
@@ -1,10 +1,17 @@
+from typing import Optional
+
 from pyteledantic.exceptions.exceptions import TelegramAPIException
 from pyteledantic.models import Bot, User
 import requests
 
+from pyteledantic.utils import proxy_handler
 
-def get_me(bot: Bot) -> User:
-    response = requests.get(f'https://api.telegram.org/bot{bot.token}/getMe')
+
+@proxy_handler
+def get_me(bot: Bot, session: Optional[requests.Session] = None, verify: bool = True) -> User:
+    if not session:
+        session = requests.Session()
+    response = session.get(f'https://api.telegram.org/bot{bot.token}/getMe', verify=verify)
     if response.status_code == 200:
         user = User(**response.json()['result'])
         return user
---
+++ pyteledantic/utils.py
@@ -0,0 +1,28 @@
+import urllib
+from requests_kerberos import HTTPKerberosAuth
+from urllib3.util import parse_url
+import requests
+from requests.exceptions import ProxyError
+
+
+class HTTPAdapterWithProxyKerberosAuth(requests.adapters.HTTPAdapter):
+    def proxy_headers(self, proxy):
+        headers = {}
+        auth = HTTPKerberosAuth()
+        negotiate_details = auth.generate_request_header(None, parse_url(proxy).host, is_preemptive=True)
+        headers['Proxy-Authorization'] = negotiate_details
+        return headers
+
+
+def proxy_handler(func):
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+        except ProxyError:
+            session = requests.Session()
+            session.proxies = urllib.request.getproxies()
+            session.mount('https://', HTTPAdapterWithProxyKerberosAuth())
+            verify = False
+            result = func(*args, session=session, verify=verify, **kwargs)
+        return result
+    return wrapper
