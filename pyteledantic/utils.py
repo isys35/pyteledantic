00cdd6a1a7b572639d8f89600fb872de0e3df2d3
isys35 <isys35@mail.ru>
2022-05-30 14:15:56 +0300

    🐞 FIX: mode type

--- pyteledantic/utils.py
+++ pyteledantic/utils.py
@@ -1,23 +1,33 @@
 from typing import Optional, Union
-from pydantic import BaseModel
 import requests
+from typing import Type
+
+from pydantic import BaseModel
+from requests import Response
 
 from pyteledantic.exceptions.exceptions import TelegramAPIException
+
+
+def get_response(url: str,
+                 method: str = 'GET',
+                 params: Optional[dict] = None) -> Response:
+    session = requests.Session()
+    response = session.request(method, url, params=params)
+    if response.status_code == 200:
+        return response
+    else:
+        description = response.json()['description']
+        raise TelegramAPIException(description)
 
 
 def base_method(
         url: str,
         method: str = 'GET',
         params: Optional[dict] = None,
-        response_model: Optional[type[BaseModel]] = None) -> Union[BaseModel, bool]:
-    session = requests.Session()
-    response = session.request(method, url, params=params)
-    if response.status_code == 200:
-        if response_model:
-            response_pydantic = response_model(**response.json()['result'])
-            return response_pydantic
-        else:
-            return response.json()['result']
+        response_model: Optional[Type[BaseModel]] = None) -> Union[BaseModel, bool]:
+    response = get_response(url, method, params)
+    if response_model:
+        response_pydantic = response_model(**response.json()['result'])
+        return response_pydantic
     else:
-        description = response.json()['description']
-        raise TelegramAPIException(description)
+        return response.json()['result']
